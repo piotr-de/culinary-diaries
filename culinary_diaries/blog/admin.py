48cd78417572a18b17987b3516b14201aafed654
Piotr Damer <piotrd@czuby.net>
2020-07-03 20:34:27 +0100

    PostAdmin customization

--- culinary_diaries/blog/admin.py
+++ culinary_diaries/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+
+    def blurb(self, post):
+        return post.post_text[:100]
+
+    fields = ['post_title', 'post_text']
+    blurb.short_description = 'Post Text'
+    list_display = ['post_title', 'blurb', 'post_date']
+
+
+admin.site.register(Post, PostAdmin)
--- culinary_diaries/blog/models.py
+++ culinary_diaries/blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
     post_text = models.TextField()
-    post_date = models.DateTimeField('published')
+    post_date = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.post_title
