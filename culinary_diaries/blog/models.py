755e31d73bc5a8cd7557fb195c85f19e422fa97f
Piotr Damer <piotrd@czuby.net>
2020-07-03 17:46:00 +0100

    Post model

--- culinary_diaries/blog/models.py
+++ culinary_diaries/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=200)
+    post_text = models.TextField()
+    post_date = models.DateTimeField('published')
