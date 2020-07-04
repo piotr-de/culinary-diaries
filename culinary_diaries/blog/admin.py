8852ec6dc043f18ff8f54beed328c08fe058c623
Piotr Damer <piotrd@czuby.net>
2020-07-04 14:45:42 +0100

    Add FileField to the Post model

--- culinary_diaries/blog/admin.py
+++ culinary_diaries/blog/admin.py
@@ -5,11 +5,16 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
 
     def blurb(self, post):
-        return post.post_text[:100]
+        output = post.post_text[:100]
+        if len(post.post_text) > 100:
+            output += '...'
+        return output
 
-    fields = ['post_title', 'post_text']
     blurb.short_description = 'Post Text'
+
+    fields = ['post_title', 'post_text', 'post_image']
     list_display = ['post_title', 'blurb', 'post_date']
+    list_filter = ['post_date']
 
 
 admin.site.register(Post, PostAdmin)
--- culinary_diaries/blog/models.py
+++ culinary_diaries/blog/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
     post_text = models.TextField()
-    post_date = models.DateTimeField(default=timezone.now())
+    post_image = models.FileField(upload_to='images/', blank=True)
+    post_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.post_title
